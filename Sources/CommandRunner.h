e85507a60573d7cddc1cead59f57d9ca54674b80
Wahab1996 <113196271+Wahab1996@users.noreply.github.com>
2026-09-05 03:57:43 +0300

    Delete Sources directory

--- Sources/CommandRunner.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-typedef void (^CommandCompletion)(int exitCode, NSString *message);
-
-@interface CommandRunner : NSObject
-+ (void)runExecutableCandidates:(NSArray<NSString *> *)candidates arguments:(NSArray<NSString *> *)arguments completion:(CommandCompletion)completion;
-@end
--- Sources/SystemInfo.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface SystemInfo : NSObject
-+ (NSArray<NSArray<NSDictionary<NSString *, NSString *> *> *> *)overviewSections;
-+ (NSString *)jailbreakRoot;
-+ (BOOL)isRootless;
-@end
